68da7d822cb97f9548b520456c01eca77e35cb1b
Michael Wilson <michaelw@4sitestudios.com>
2023-08-22 23:44:59 -0400

    Incremented Version Number

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: ShareThumb
- * Version: 1.0.9
+ * Version: 1.0.10
  * Plugin URI: https://sharethumb.io/
  * Description: Configure the ShareThumb service directly via your own website.
  * Author: 4Site Interactive Studios
